45f17110983305eee34a08fb0b0fc8a8884fa6a0
PauloEduardoCruz <pauloeduardo.crp@gmail.com>
2020-12-09 00:01:20 -0300

    Criando repositório

---
+++ src/App.vue
@@ -0,0 +1,132 @@
+<template>
+  <div id="app">
+
+
+    <div class="grid-master">
+      <div v-for="photo in filterMarsRover" :key="photo.img_src">
+        <PhotoMarte :img="photo.img_src" :idNasa="photo.id" :camera="photo.camera.full_name"
+         :roverNome="photo.rover.name" :status="photo.rover.status"
+         :lancamento="photo.rover.launch_date" :chegada="photo.rover.landing_date" />
+      </div>
+    </div>
+
+    <button @click="buscar()" class="button buscaBtn is-success">Atualizar</button>
+
+    <p class="links"> 
+        <a href="https://www.linkedin.com/in/paulo-eduardo-s-da-cruz-3773bb154/">Dev by Paulo Cruz /</a>
+        <a href="https://vuejs.org/"> Vue.js /</a>
+        <a href="https://api.nasa.gov/"> Nasa - Mars Rover Api </a>
+    </p>
+
+  </div>
+</template>
+
+<script>
+
+  import axios from 'axios';
+
+  import PhotoMarte from './components/PhotoMarte'
+
+  export default {
+    name: 'App',
+    components: {
+      PhotoMarte
+    },
+    data(){
+      return {
+        marsRover: [],
+        filterMarsRover: [],
+        numeros: [],
+      }
+    },
+    created: function(){
+
+      this.aleat();
+      //axios.get("qqq")
+      
+      axios.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1500&api_key=DEMO_KEY").then(res => {
+          this.marsRover = res.data.photos
+          for(let i in this.numeros){
+            this.filterMarsRover.push(this.marsRover[this.numeros[i]])
+          }
+          console.log(this.marsRover)
+          console.log(this.filterMarsRover)
+      })
+    
+    },
+
+
+
+    methods: {
+      buscar: function(){
+        this.aleat();
+
+        this.filterMarsRover = []
+          for(let i in this.numeros){
+            this.filterMarsRover.push(this.marsRover[this.numeros[i]])
+          }
+      },
+
+
+
+
+      aleat: function(){
+        this.numeros= [];
+        while (this.numeros.length < 2) {
+          var aleatorio = Math.floor(Math.random() * 200);
+          if (this.numeros.indexOf(aleatorio) == -1)
+              this.numeros.push(aleatorio);
+        }
+        console.log("Aleatorios: ",this.numeros)
+      }
+
+
+    },
+    computed: {
+
+    }
+  }
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+
+  min-height: 100vh;
+
+    background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(255, 255, 255, 0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgb(0 0 0) 120%) #505050;
+    background-blend-mode: multiply,multiply;
+}
+
+
+.grid-master{
+  min-height: 90vh;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+}
+
+a {
+  margin-top: 100px;
+  color: inherit  !important; /* blue colors for links too */
+  text-decoration: inherit !important; /* no underline */
+}
+.links{
+  text-align: start;
+}
+
+
+@media only screen and (max-width: 700px){
+    .grid-master{
+        display: flex;
+        flex-direction: column;
+    } 
+}
+</style>
---
+++ src/components/PhotoMarte.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+
+        <div class="cartao">
+            <div class="">
+                <figure class="figura">
+                    <img class="imgg" :src="img" alt="Placeholder image">
+                </figure>
+            </div>
+
+            <div class="grid-tx">
+                <div class="grid-titulo">
+                    <p >{{camera}} </p> <br>
+                    <p class="id"> ID: {{idNasa}} </p>
+                </div>
+
+                <div class="tx">
+                    <p>Rover name: {{roverNome}} </p>
+                    <p>Status: {{status}}</p>
+                    <p>Lançamento: {{lancamento}}</p>
+                    <p>Chegada: {{chegada}} </p>
+                </div>
+            </div>
+
+        </div>
+
+  </div>
+</template>
+
+
+<script>
+
+
+export default {
+    props:{
+        img: String,
+        idNasa: Number,
+        camera: String,
+        roverNome: String,
+        status: String,
+        lancamento: String,
+        chegada: String
+    }
+}
+</script>
+
+
+<style>
+.cartao{
+
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+
+    border-radius: 0.5rem;
+    background-color: #5e5e5e00;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    color: #ffffff;
+    width: 30vw;
+    height: auto;
+    overflow: hidden;
+    position: relative;
+
+
+    border-bottom: 3px solid#48c774;
+    border-top: 1px solid#ffffff;
+    
+}
+
+
+.grid-titulo{
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 2px solid#48c774;
+  
+}
+
+p {
+    display: block;
+    margin-block-start: 0em !important;
+    margin-block-end: 0em !important;
+    margin-inline-start: 0px !important;
+    margin-inline-end: 0px !important;
+}
+
+.tx{
+    text-align: initial;
+    margin-top: 10%;
+}
+
+.imgg {
+    height: 100%;
+    width: 100%;
+    object-fit: cover; 
+}
+
+.figura{
+    width: 100%;
+    height: 50vh;
+}
+
+.grid-tx{
+    padding: 5% 10%;
+}
+
+.id{
+    color: #48c774;
+}
+
+
+
+@media only screen and (max-width: 700px){
+    .cartao {
+        width: 95vw;
+        margin-bottom: 10%;
+    }
+}
+</style>
